325bebb6ba00c6ba1c5c32e297411d0c003533a0
Dinh-Son <vudinhso@gmail.com>
2026-04-28 19:06:27 +0700

    Fix .gitignore and remove tracked files

--- build/rmitbot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/v120506/03-ROS2_Workspace/rmitbot_v5/lesson1_ws/src/rmitbot_description
-# Build directory: /home/v120506/03-ROS2_Workspace/rmitbot_v5/lesson1_ws/build/rmitbot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
